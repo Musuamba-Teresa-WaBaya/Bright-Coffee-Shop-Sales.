d60579056a6729f36ad69c4cd423eb23790a133b
Musuamba Teresa Wa Baya <wabayamusuamba@gmail.com>
2026-06-13 19:07:37 +0200

    Update SQL Code
    Refactor SQL queries for exploratory data analysis, including updates to table names, added comments, and enhanced data checks.

--- 3. Data Processing/Data Exploratory Analysis.sql
+++ 3. Data Processing/Data Exploratory Analysis.sql
@@ -1,102 +1,124 @@
---I want to see my table in the coding environment and start exploring each columnn 
---This process is called Data Exploratory Analysis 
+--------------------------------------------------------------------------------------------
+--BRIGHT COFFEE SHOP CODING
+--------------------------------------------------------------------------------------------
+--Exploratory Data Analysis 
 
-select * 
-from `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`
-limit 10;
+--1. Running the entire table 
 
---Now i need to understand what is contained within my data 
------------------------------------------------------------------------------
---1. Checking the Date Range (To understand when they started collecting this data)
---------------------------------------------------------------------
---Results show that they started collecting this data in 2023-01-01
---Now we know that this data that i have been given is historical records from 6 months, and it starts from Jan - June. 
-SELECT MIN(transaction_date) AS Min_date 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+SELECT * 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` 
+LIMIT 100;
 
---Results show that they last collected data in 2023-06-30
-SELECT MAX(transaction_date) AS Max_date 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--2. Checking the date range - This data is collected over 6 months, from Jan -June in 2023
+SELECT
+    MIN (transaction_date) AS minimum_Date, 
+    MAX (transaction_date) AS Maximum_Date
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
-----------------------------------------------------------------------------
---2. Checking the names of the different Stores 
---------------------------------------------------------------------
---Results show that i just have 3 storesand their names are Lower Manhattan, Hell's Kitchen, and Astoria
-SELECT DISTINCT (store_location)
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--3. Checking the different store locations - we have 3 diferent locations 
+SELECT DISTINCT store_location
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
-SELECT COUNT(DISTINCT (store_location))
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--4 Checking the unique product categories sold at our stores - 9 categories 
+SELECT DISTINCT product_category
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
-----------------------------------------------------------------------------
---3. Checking Products Sold( Now we want to understand what exactly the shop is selling based on the product categories)  
---------------------------------------------------------------------
---Results show that i have 9 different categories of products being sold, they have different groups of products. 
-SELECT DISTINCT (product_category) 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--5 Checking the unique product Types - 29 product types 
+SELECT DISTINCT product_type
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
+
+--6 Checking the unique product detail - we have 80 differnt kinds of products 
+SELECT DISTINCT product_detail 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
 
-----------------------------------------------------------------------------
---3. Checking the product details to better understand the contents of that column 
---------------------------------------------------------------------
---Results show that 80 different details 
-SELECT DISTINCT product_detail
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--7. Checking for NULL/Missing values in our data in various columns 
+--(NULL values skew our data, and its important to check for them) - no rows returns, so there are no missing values. 
+--If there were missing values, we would follow imputation methods, such as using average values, replacing with 0 etc - but this has to be done with und4erstanding. 
 
---Now here we want to know which product categories correspond with which product details 
---we see the 9 categories and how they correspond wit the different product details / names 
---Eg now i can understand what is contained in my data , eg  "Lemon grass is a Tea" and "Ethiopia is a Coffee bean"
-SELECT DISTINCT product_category, 
-                product_detail AS product_names
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`
-ORDER BY product_category DESC;
+SELECT * 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` 
+WHERE unit_price IS NULL
+OR transaction_qty IS NULL
+OR transaction_date IS NULL ; 
 
-----------------------------------------------------------------------------
---4. Checking prices 
---------------------------------------------------------------------
---Results show that the cheapest price is 0.8 and the second query  shows us the cheapest prices in each of the 9 categories
-SELECT MIN(unit_price) AS Cheapest_Price 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--8. Checking the lowest and highest Unit price - The lowest and highest prices are 0.8 aand 45 respectively. 
+SELECT
+    MIN (unit_price) AS Lowest_unit_price, 
+    MAX (unit_price) AS Hightest_unit_price
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
---The price of the cheapest product per category 
-SELECT product_category, MIN(unit_price) AS cheapest_price
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`
-GROUP BY product_category
-ORDER BY cheapest_price ASC;
+--9. Extracting the day and Month Names -identified the day and month name per transaction date 
+SELECT 
+    transaction_date, 
+    Dayname (transaction_date) as Day_name, 
+    Monthname(transaction_date) as Month_name 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
---The most expensive item costs 45, and the second queries show the most expensive prices per category. 
--- Here i can say that the most expensive Branded item costs 28 and the cheapest costs 12
-SELECT MAX(unit_price) AS most_expensive_price 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--10. Calculating the revenue 
+SELECT 
+    unit_price, transaction_qty, 
+    unit_price * transaction_qty AS Revenue 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1` ; 
 
---The price of the cheapest product per category 
-SELECT product_category, MAX(unit_price) AS most_expensive_price 
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`
-GROUP BY product_category
-ORDER BY most_expensive_price  ASC;
+--11. Calcalte revenue using aggregation 
+SELECT 
+    SUM(unit_price*transaction_qty) AS Revenue 
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1`;    
 
-----------------------------------------------------------------------------
---4. Checking the size  of the Data (to see if there are any duplicates in the data) 
---------------------------------------------------------------------
---Because the number of rows of the transaction ID  is the same as the total number of rows, i know that my data has no duplicates 
--- so the data has 14116 rows 
-SELECT COUNT(*) AS number_of_rows,
-       COUNT (Distinct transaction_id) AS number_of_sales
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
 
-----------------------------------------------------------------------------
---4.Counting more , exploring the full shape of our data 
---------------------------------------------------------------------
-SELECT COUNT(*) AS number_of_rows,
-       COUNT (Distinct transaction_id) AS number_of_sales,
-       COUNT (Distinct store_id) AS number_of_stores,
-       COUNT (Distinct store_location) AS number_of_store_locations,
-       COUNT (Distinct product_id) AS number_of_products,
-       COUNT (Distinct product_category) AS number_of_product_categories,
-       COUNT (Distinct product_type) AS number_of_product_types,
-       COUNT (Distinct product_detail) AS number_of_product_details
-FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis`;
+--Conclusion - Now that we understand our data from the EDA, we are ready to move to the step of Data Cleaning, inorder to then analyse our data
 
---Conclusion - Now that we understand our data in what we have done above which is called the Data Exploratory Analysis, we are ready to move to the step of Data Cleaning 
---We need to start checking the features . we have dipped into the data, we understand what we are given, we have explored what is contained in our data without making any changes 
---Now we can start moving to a point where we can start analysing the data. 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--12. Running 1 Big Query that combines all functions, inorder to get a fully comprhensive dataset. 
+
+--Same as doing select * to run and check all the columns 
+SELECT 
+    transaction_id, 
+    transaction_date, 
+    transaction_time, 
+    transaction_qty, 
+    store_id, 
+    store_location, 
+    product_id, 
+    unit_price, 
+    product_category, 
+    product_type, 
+    product_detail,
+
+---After visualising the given columns, we add new columns to enhance our analysis. 
+--After we have added , we can then move the columns around in our select statement inorder to see information flow as desired
+
+--New Columns 1,2,3 - 
+   Dayname (transaction_date) as Day_name, 
+   Monthname(transaction_date) as Month_name, 
+   Dayofmonth(transaction_date) AS Date_of_month,
+
+--New Columns 4- Adding a weekend / weekday CASE statement called Day_Classification 
+CASE 
+    WHEN  Dayname (transaction_date)  IN ('Sun', 'Sat') THEN 'Weekend' 
+    ELSE 'Weekday'
+END AS Day_classification,
+
+--New Columns 5 - Adding Time Buckets to extract the time from the time stamp in the transaction time column 
+CASE 
+    WHEN date_format (transaction_time, 'HH:MM:SS') BETWEEN   '05:00:00' AND '08:59:99' THEN '01.Rush Hour'
+    WHEN date_format (transaction_time, 'HH:MM:SS') BETWEEN  '09:00:00' AND '11:59:59' THEN '02.Mid Morning'
+    WHEN date_format (transaction_time, 'HH:MM:SS') BETWEEN  '12:00:00' AND '15:59:59' THEN '03.Afternoon'
+    WHEN date_format (transaction_time, 'HH:MM:SS') BETWEEN  '16:00:00' AND '18:00:00' THEN '04.Evening'
+    ELSE '05.Night'
+END AS Time_classification, 
+    
+--New Column 6 - Adding Spend Buckets based on our min of 0.8 and max of 340
+CASE 
+    WHEN (transaction_qty*unit_price) <=50 THEN '01. Low Spender'
+    WHEN (transaction_qty*unit_price) BETWEEN 51 AND 200 THEN '02. Medium Spender'
+    WHEN (transaction_qty*unit_price) BETWEEN 201 AND 300 THEN '03. High Spender'
+    ELSE '04.Ultra Spender'
+END AS spend_bucket,
+
+--New Column 7 - Adding Revenue Column 
+--Download the dataset and sum the entire revenue column, the total needs tp be 698812.3299 inorder for you to know that you got all the data in your table. 
+transaction_qty*unit_price AS Revenue 
+
+FROM `workspace`.`default`.`case_study_1_bright_coffee_shop_analysis_case_study_1`;    
